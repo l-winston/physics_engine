4be9ac58a29f977c2387fc903ffdadb2a552e165
Winston Liu <winwinstonliu@gmail.com>
2017-12-06 17:27:09 -0800

    fixed white dot problem

--- src/Box.java
+++ src/Box.java
@@ -21,60 +21,21 @@ public class Box extends Entity {
 	}
 
 	public void update() {
-		int image_width = image.getWidth();
-		int image_height = image.getHeight();
+		updateLocation();
+		updateDisplay();
+	}
 
-		if (exists) {
-			for (double x = -width / 2; x < width / 2; x++) {
-				for (double y = -height / 2; y < width / 2; y++) {
-					Point v = new Point(x, y);
-					v.getPolar();
-					v.y += this.direction;
-
-					while (v.y > 2 * Math.PI) {
-						v.y -= 2 * Math.PI;
-					}
-
-					v.getUnitVectors();
-					v.x += this.x;
-					v.y += this.y;
-
-					Color color = this.color;
-					double i_x = v.x + image_width / 2;
-					double i_y = image_height / 2 - 1 - v.y;
-
-					int i_x_floor = (int) Math.floor(i_x);
-					int i_x_ceil = (int) Math.ceil(i_x);
-					int i_y_floor = (int) Math.floor(i_y);
-					int i_y_ceil = (int) Math.ceil(i_y);
-
-					try {
-						paintPoint(i_x_floor, i_y_floor, color);
-						paintPoint(i_x_ceil, i_y_ceil, color);
-					} catch (ArrayIndexOutOfBoundsException e) {
-						// if it is impossible for a pixel of the Entity to be
-						// on the screen, do not render it
-						double corner_max = Math.sqrt(Math.pow(width / 2, 2) + Math.pow(height / 2, 2));
-						if (this.x + corner_max < -image_width / 2 || this.x - corner_max > image_width / 2) {
-							exists = false;
-						}
-						if (this.y + corner_max < -image_height / 2 || this.y - corner_max > image_height / 2) {
-							exists = false;
-						}
-					}
-				}
-			}
-		}
-		
-
+	private void updateLocation() {
 		for (Entity e : PhysicsMain.entities) {
-				//velocity.y -= 1 / Math.pow((y - e.y), 2) * (y - e.y) / Math.abs((y - e.y));// gravitation
-				//velocity.x -= 1 / Math.pow((x - e.x), 2) * (x - e.x) / Math.abs((x - e.x));
-				
-			//this.velocity.y += (e.y-this.y)/100;
-				//this.velocity.x += (e.x-this.x)/100;
+			// velocity.y -= 1 / Math.pow((y - e.y), 2) * (y - e.y) /
+			// Math.abs((y - e.y));// gravitation
+			// velocity.x -= 1 / Math.pow((x - e.x), 2) * (x - e.x) /
+			// Math.abs((x - e.x));
+
+			// this.velocity.y += (e.y-this.y)/100;
+			// this.velocity.x += (e.x-this.x)/100;
 		}
-			
+
 		this.x += this.velocity.x;
 		this.y += this.velocity.y;
 		// velocity.y -= (y-100)/100.0;// gravitation
@@ -84,6 +45,57 @@ public class Box extends Entity {
 		this.direction += this.angular_velocity;
 		while (direction > 2 * Math.PI) {
 			direction -= 2 * Math.PI;
+		}
+	}
+
+	private void updateDisplay() {
+		if (!exists) return;
+		int image_width = image.getWidth();
+		int image_height = image.getHeight();
+
+		for (double x = -width / 2; x < width / 2; x++) {
+			for (double y = -height / 2; y < width / 2; y++) {
+				Point v = new Point(x, y);
+				v.getPolar();
+				v.y += this.direction;
+
+				while (v.y > 2 * Math.PI) {
+					v.y -= 2 * Math.PI;
+				}
+
+				v.getUnitVectors();
+				v.x += this.x;
+				v.y += this.y;
+
+				Color color = this.color;
+				double i_x = v.x + image_width / 2;
+				double i_y = image_height / 2 - 1 - v.y;
+
+				int i_x_floor = (int) Math.floor(i_x);
+				int i_x_ceil = (int) Math.ceil(i_x);
+				int i_y_floor = (int) Math.floor(i_y);
+				int i_y_ceil = (int) Math.ceil(i_y);
+
+				try {
+					paintPoint(i_x_floor, i_y_floor, color);
+					paintPoint(i_x_ceil, i_y_ceil, color);
+
+					paintPoint(i_x_floor - 1, i_y_floor, color);
+					paintPoint(i_x_floor, i_y_floor - 1, color);
+					paintPoint(i_x_ceil + 1, i_y_ceil, color);
+					paintPoint(i_x_ceil, i_y_ceil + 1, color);
+				} catch (ArrayIndexOutOfBoundsException e) {
+					// if it is impossible for a pixel of the Entity to be
+					// on the screen, do not render it
+					double corner_max = Math.sqrt(Math.pow(width / 2, 2) + Math.pow(height / 2, 2));
+					if (this.x + corner_max < -image_width / 2 || this.x - corner_max > image_width / 2) {
+						exists = false;
+					}
+					if (this.y + corner_max < -image_height / 2 || this.y - corner_max > image_height / 2) {
+						exists = false;
+					}
+				}
+			}
 		}
 	}
 
--- src/PhysicsMain.java
+++ src/PhysicsMain.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public class PhysicsMain {
 	public static JFrame frame = new JFrame("Physics Engine");
-	public static BufferedImage image = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
+	public static BufferedImage image = new BufferedImage(750, 1000, BufferedImage.TYPE_INT_RGB);
 	public static ArrayList<Entity> entities = new ArrayList<Entity>();
 
 	public static void main(String[] args) {
@@ -16,9 +16,9 @@ public class PhysicsMain {
 		frame.pack();
 
 		setWhite();
-		Box red = new Box(50, 50, new Point(0, 0), 50, 250, 100.0, new Color(255, 0, 0), Math.toRadians(45), Math.toRadians(5), true);
+		Box red = new Box(50, 50, new Point(0, 0), 50, image.getHeight()/2, 100.0, new Color(255, 0, 0), Math.toRadians(45), Math.toRadians(5), true);
 		entities.add(red);
-		Box green = new Box(50, 50, new Point(0, 0), -50, 250, 100.0, new Color(0, 255, 0), Math.toRadians(0), Math.toRadians(2), true);
+		Box green = new Box(50, 50, new Point(0, 0), -50, image.getHeight()/2, 100.0, new Color(0, 255, 0), Math.toRadians(0), Math.toRadians(2), true);
 		entities.add(green);
 
 		//Box blue = new Box(50, 50, new Vector(0, 5), 0, -100, 100.0, new Color(0, 0, 255), Math.toRadians(0), Math.toRadians(0.5), true);
@@ -32,7 +32,7 @@ public class PhysicsMain {
 	public static void setWhite() {
 		for (int i = 0; i < image.getHeight(); i++) {
 			for (int j = 0; j < image.getWidth(); j++) {
-				image.setRGB(i, j, new Color(255, 255, 255).getRGB());
+				image.setRGB(j, i, new Color(255, 255, 255).getRGB());
 			}
 		}
 		image.setRGB(image.getWidth()/2, image.getHeight()/2, new Color(0, 0, 255).getRGB());
